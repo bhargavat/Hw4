ccf8548703a807b91aa08d93f5ccdc431c1394ec
bhargavat <bhargava.ramisetty@gmail.com>
2016-11-08 20:16:56 -0800

    Setup files added
    composer.json file, config, and vendor directory added

---
+++ src/configs/config.php
@@ -0,0 +1,13 @@
+<?php
+class config
+{
+    const Servername = "localhost";
+    const Username = "root";
+    const Password = "";
+    const DbName = "HW4";
+    const Mycon = '';
+
+    const HW3root = "http://localhost/Hw4";
+}
+
+?>
